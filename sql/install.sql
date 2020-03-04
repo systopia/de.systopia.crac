3118ffb76a19dd279f9ec09f9b593d4c22897a5d
B. Endres <endres@systopia.de>
2020-03-04 18:36:06 +0100

    adjust to STRICT DB settings

--- sql/install.sql
+++ sql/install.sql
@@ -1,10 +1,10 @@
 -- central data structure recording the timestamps
 CREATE TABLE IF NOT EXISTS `civicrm_resource_access` (
-     `entity_id`          int unsigned        COMMENT 'entity resource being used/edited',
-     `entity_table`       varchar(128)        COMMENT 'entity resource being used/edited',
-     `contact_id`         int unsigned        COMMENT 'FK to Contact ID that is using/editing the resource',
-     `using_since`        timestamp           COMMENT 'when did the editing of that resource start?',
-     `timeout`            timestamp           COMMENT 'when will the resource time out',
+     `entity_id`          int unsigned      COMMENT 'entity resource being used/edited',
+     `entity_table`       varchar(128)      COMMENT 'entity resource being used/edited',
+     `contact_id`         int unsigned      COMMENT 'FK to Contact ID that is using/editing the resource',
+     `using_since`        timestamp    NULL COMMENT 'when did the editing of that resource start?',
+     `timeout`            timestamp    NULL COMMENT 'when will the resource time out',
     PRIMARY KEY (entity_id,entity_table,contact_id),
     INDEX `timeout` (timeout),
     CONSTRAINT FK_civicrm_crac_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE CASCADE
